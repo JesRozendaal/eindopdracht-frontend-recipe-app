efb8c9bb88fed415a127cfce61fee9a62164e380
Jessica Rozendaal <remyenjessica@gmail.com>
2022-04-24 16:08:31 +0200

    start-en-api-gekoppeld

--- src/components/recipes/Recipe2.js
+++ src/components/recipes/Recipe2.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Recipe2.css';
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+// endpoint: https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
 
 const Recipe2 = () => {
+    const[recipe, setRecipe] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=${process.env.REACT_APP_API_KEY}`);
+                console.log(result);
+                setRecipe(result);
+            }catch (e) {
+                console.error(e);
+            }
+        }
+        fetchData();
+    },[]);
 
         return (
             <>
-
+                <h2>Your recipes</h2>
+                <div className="container-recipe2">
+                    <h3><Link to={`/recipes/${recipe.data[0].id}`} className="link-recipe">{recipe.data[0].title}</Link></h3>
+                </div>
                 </>
     );
 };
--- src/pages/fridge/Fridge.js
+++ src/pages/fridge/Fridge.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import './Fridge.css';
+import Header from "../../components/header/Header";
+import Recipe2 from "../../components/recipes/Recipe2";
+import {Link} from "react-router-dom";
 
 const Fridge = () => {
+
     return (
         <div>
+            <Header
+                title="What's in your fridge?"
+                className="header-fridge"
+            />
 
+            <main>
+                <div className="outer-container">
+                    <div className="inner-container">
+                        <h2>Stop wasting food!</h2>
+                        <p>Just type the food left in your fridge in the bar below and receive recipes for a delicious meal.</p>
+                        <p>If you like to save your favorite recipes, just <Link to="/login-register">log in or register</Link></p>
+
+                        <form>
+                            <label htmlFor="ingredients">
+                                <p>What's in your fridge?</p>
+                                <input
+                                    type="text"
+                                    id="ingredients"
+                                />
+                            </label>
+                            <button type="button">
+                                Search
+                            </button>
+                        </form>
+
+                        <Recipe2 />
+
+
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
