5a9fbfdc24efe99351545c8d2a64a76fe5598ffe
Jessica Rozendaal <remyenjessica@gmail.com>
2022-04-25 12:57:28 +0200

    mapping-fridge-pagina

--- src/components/recipes/Recipe2.js
+++ src/components/recipes/Recipe2.js
@@ -22,31 +22,20 @@ const Recipe2 = () => {
     },[]);
 
         return (
-            // <>
-            //     <h2>Your recipes</h2>
-            //     {recipe &&
-            //     recipe.results.map((recipes) => {
-            //         return(
-            //             <div className="container-recipe3">
-            //                 <img src={recipes.image} alt="recipe" className="image-recipe3"/>
-            //                 <h3><Link to={`/recipes/${recipes.id}`} className="link-recipe">{recipes.title}</Link></h3>
-            //             </div>
-            //         )})
-            //     }
-            // </>
-
             <>
                 {recipe &&
-                    <>
-                        <h2>Your recipes</h2>
-                        <div className="container-recipe2">
-                            <img src={recipe.data[0].image} alt="recipe" className="image-recipe2"/>
-                            <h3><Link to={`/recipes/${recipe.data[0].id}`} className="link-recipe">{recipe.data[0].title}</Link></h3>
-                        </div>
-                    </>
+                <>
+                    <h2>Your recipes</h2>
+                    {recipe.data.map((recipes) => {
+                        return(
+                            <div className="container-recipe2">
+                                <img src={recipes.image} alt="recipe"/>
+                                <h3><Link to={`/recipes/${recipes.id}`} className="link-recipe">{recipes.title}</Link></h3>
+                            </div>
+                        )})}
+                </>
                 }
             </>
-
     );
 };
 
